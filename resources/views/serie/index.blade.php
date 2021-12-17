23c785b1ddbb76f6f3dfad20bba49c930c157281
Mohammed Pombo <mohammed_pombo@ens.univ-artois.fr>
2021-12-17 12:08:20 +0100

    preparation demo

--- resources/views/serie/index.blade.php
+++ resources/views/serie/index.blade.php
@@ -18,24 +18,25 @@
         </nav>
     </div>
 
-<div class="gridListe">
+<div class="gridIndex">
             
             <div class="listeGenres">
                 <div>Genres</div>
-                 <ul> 
+                 <ul>
 
-                    <li>Action</li>
-                    <li>Action</li>
-                    <li>Action</li>
-                    <li>Action</li>
-                    <li>Action</li>
-                    <li>Action</li>
-                    <li>Action</li>
-                    <li>Action</li>
-                    <li>Action</li>
-                    <li>Action</li>
-                    <li>Action</li>
-                    <li>Action</li>
+                    <li><bouton class="bouton" onclick="window.location.href ='/serie?genre=Mystery';"> mystery</bouton></li>
+                    <li><bouton class="bouton" onclick="window.location.href ='/serie?genre=Crime';"> Crime </bouton></li>
+                     <li><bouton class="bouton" onclick="window.location.href ='/serie?genre=Horror';"> Horror </bouton></li>
+                    <li><bouton class="bouton" onclick="window.location.href ='/serie?genre=Fantasy';"> Fantasy </bouton></li>
+                    <li><bouton class="bouton" onclick="window.location.href ='/serie?genre=Legal';"> Legal </bouton></li>
+                    <li><bouton class="bouton" onclick="window.location.href ='/serie?genre=Thriller';"> Thriller </bouton></li>
+                    <li><bouton class="bouton" onclick="window.location.href ='/serie?genre=Romance';"> Romance </bouton></li>
+                    <li><bouton class="bouton" onclick="window.location.href ='/serie?genre=Science-Fiction';"> Science-Fiction </bouton></li>
+                    <li><bouton class="bouton" onclick="window.location.href ='/serie?genre=Supernatural';"> Supernatural </bouton></li>
+                    <li><bouton class="bouton" onclick="window.location.href ='/serie?genre=Western';"> Western </bouton></li>
+                    <li><bouton class="bouton" onclick="window.location.href ='/serie?genre=Drama';"> Drama </bouton></li>
+                    <li><bouton class="bouton" onclick="window.location.href ='/serie?genre=Anime';"> Anime</bouton></li>
+                     <li><bouton class="bouton" onclick="window.location.href ='/serie?genre=Family';"> Family </bouton></li>
                 </ul>
     </div>
     <div class="new">
