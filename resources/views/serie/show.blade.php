9c15fd1027e7cdfea7ee60539ffcebf6d8c46469
Maxime Carlier <maxime_carlier@ens.univ-artois.fr>
2021-12-17 12:01:15 +0100

    affichage des bouton revoir, continuer et regarder

--- resources/views/serie/show.blade.php
+++ resources/views/serie/show.blade.php
@@ -18,10 +18,21 @@
 
     @auth
         @if(Auth::user()->isSeenSerie($serie->id))
-            <h3 class="vu">Vu</h3>
+            <h3 class=""vu>Vu</h3>
+            <button onclick="window.location.href='/serie/{{$serie->id}}?review=1'" class="mrq">Le revoir</button>
         @else
-            <button onclick="window.location.href='/serie/check/{{$serie->id}}'" class="mrq">Marquer comme lu</button>
+            <div>
+                <button onclick="window.location.href='/serie/check/{{$serie->id}}'" class="mrq">Marquer comme vu</button>
+                <button onclick="window.location.href='{{route('serie.episode',['serie_id'=>$serie->id,'episode_id' => Auth::user()->getCurrentEpisode($serie->id)->id])}}'" class="mrq">
+                    @if(Auth::user()->getCurrentEpisode($serie->id)->numero=='1' && Auth::user()->getCurrentEpisode($serie->id)->saison=='1')
+                        Regarder
+                    @else
+                        Continuer
+                    @endif
+                </button>
+            </div>
         @endif
+
     @endauth
     </div>
     @foreach($saisons as $saison)
@@ -31,8 +42,12 @@
             @foreach($saison as $episode)
                 <ul>
                     <li class="nomEpisode"><h3>{{$episode->numero}} {{$episode->nom}}</h3></li>
-                    <li class="imgEpisode"><img src="{{URL::asset($episode->urlImage)}}"/></li>
+                    <li class="imgEpisode"><a @auth href="{{route('serie.episode',['serie_id'=>$episode->serie_id, 'episode_id' => $episode->id])}}" @endauth>
+                            <img src="{{URL::asset($episode->urlImage)}}"/>
+                        </a></li>
+                    @auth @if(Auth::user()->seenEpisode($episode->id)) <li>Vu</li> @endif @endauth
                 </ul>
+
             @endforeach
         </div>
     @endforeach
